c58ae58de3520bffcd94aed5e9c186e5294c2dd3
shady <shadyrajaab@gmail.com>
2024-06-03 16:40:50 -0300

    feat: adicionar equipe valparaíso e samambaia

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from datetime import datetime, time
 import pandas as pd
 import requests
 
-from utils.variables import CONSULTORES, LOGIN_URL, PAYLOAD, RELATORIO_URL
+from utils.variables import (FREECEL, LOGIN_URL, PAYLOAD, RELATORIO_URL,
+                             VALPARAISO)
 
-date = datetime(2024, 4, 15)
+date = datetime.now()
 
 start_date = int(datetime.combine(date, time.min).timestamp()) * 1000
 end_date = int(datetime.combine(date, time.max).timestamp()) * 1000 - 1
 
-RECORDS = []
 
 
 def login_vivo_gestao(url: str, payload: dict):
@@ -21,54 +21,45 @@ def login_vivo_gestao(url: str, payload: dict):
     return json["sessionId"], json["remoteIp"]
 
 
-def get_total_record_count(url: str, consultor: str, telefone: str):
+def get_total_record_count(url: str, consultor: str, telefone: str, RECORDS:list):
     response = requests.get(url)
     json = response.json()
-    called_numbers = []
-    call_to_any_consul_number = False
-    if json["totalRecordCount"] > 0:
-        for call in json["result"]:
-            called_number = call["calledMsisdn"]
-            if called_numbers in list(CONSULTORES.values()):
-                called_numbers.append(called_number)
-                call_to_any_consul_number = True
-
     result = {
         "CONSULTOR": consultor,
         "TELEFONE": telefone,
         "CHAMADAS": json["totalRecordCount"],
-        "LIGOU PARA OUTRO CONSULTOR": call_to_any_consul_number,
-        "QUEM RECEBEU A LIGAÇÃO": str(called_numbers)
     }
     RECORDS.append(result)
 
+def get_threads(consultores: dict):
+    RECORDS = []
+    threads = []
+    for consultor, telefone in consultores.items():
+        url = RELATORIO_URL.format(
+            telefone=telefone,
+            startDate=start_date,
+            endDate=end_date,
+            sessionId=session_id,
+            remoteIp=remote_ip,
+        )
+        thread = threading.Thread(
+            target=get_total_record_count,
+            args=(
+                url,
+                consultor,
+                telefone,
+                RECORDS
+            ),
+        )
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
+
+    return pd.DataFrame(RECORDS)
 
 session_id, remote_ip = login_vivo_gestao(LOGIN_URL, PAYLOAD)
 
-threads = []
-for consultor, telefone in CONSULTORES.items():
-    url = RELATORIO_URL.format(
-        telefone=telefone,
-        startDate=start_date,
-        endDate=end_date,
-        sessionId=session_id,
-        remoteIp=remote_ip,
-    )
-    thread = threading.Thread(
-        target=get_total_record_count,
-        args=(
-            url,
-            consultor,
-            telefone,
-        ),
-    )
-    thread.start()
-    threads.append(thread)
-
-
-for thread in threads:
-    thread.join()
-
-
-dataframe = pd.DataFrame(RECORDS)
-dataframe.to_excel(f"relatorio chamadas {date.strftime("%d-%m-%Y")}.xlsx")
+get_threads(FREECEL).to_excel(f"relatorio chamadas {date.strftime("%d-%m-%Y")}.xlsx", index=False)
+get_threads(VALPARAISO).to_excel(f"relatorio chamadas {date.strftime("%d-%m-%Y")} valparaiso.xlsx", index=False)
